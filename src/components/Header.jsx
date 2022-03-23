8fad64d3a810b6c974878e8f01a4739c2efe4def
pruebas <pruebas@mail.com>
2022-03-22 21:25:38 -0500

    Trabajando en animaciones de titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export const Header = () => {
-    const title = `HOLA, SOY JORGE VELASQUEZ, DESAROLLADOR WEB`;
+    
+    const title = ['HOLA, ', 'SOY', 'JORGE VELASQUEZ', 'DESAROLLADOR WEB'];
+    const saludo = 'HOLA, ';
+
     return (
-        <header className='title-content'>
+        <header className="title-content">
             <h1 className="title">
-                {title.split('').map((chart, index) => (
-                    <span className="blast" key={index}>{chart}</span>
+                {title.map((charts) => (
+                    <>
+                        {charts.split("").map((chart, index) => (
+                            <span className="blast" key={index}>
+                                {chart}
+                            </span>
+                        ))}
+                        <br />
+                    </>
                 ))}
             </h1>
         </header>
